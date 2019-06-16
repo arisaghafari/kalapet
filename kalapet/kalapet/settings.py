4a15e25ad79a8c0fa7db4daba09f360b335cad04
arisaghafari <arisa_ghafari@outlook.com>
2019-06-16 15:11:18 +0430

    Add allowed hosts

--- kalapet/kalapet/settings.py
+++ kalapet/kalapet/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = '*zzs#1#kk8#yg7arv67fgywn*qbj#p!z3lsm&ua&*q)+f$36ec'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['arisaghafari.pythonanywhere.com',
+                 '127.0.0.1']
 
 
 # Application definition
